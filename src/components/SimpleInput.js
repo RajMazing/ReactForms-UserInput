0f8b65e55165583594af41a1b45a2caa0434b442
RajMazing <caseyraj92@gmail.com>
2022-07-18 16:40:04 -0500

    using validation for changed state in every KEY STROKE. - Need to REFACTOR code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,22 +3,31 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
 	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState("");
-	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
 	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+
 
 	const nameInputChangeHandler = (event) => {
 		setEnteredName(event.target.value);
+
+		if (event.target.value.trim() !== '') {
+			setEnteredNameIsValid(true)
+		}
 	};
+	const nameInputBlurHandler = (events) => {
+		setEnteredNameTouched(true)
+		
+		if (enteredName.trim() === "") {
+			setEnteredNameIsValid(false);
+		}
+	}
 
 	const formSubmissionHandler = (event) => {
 		event.preventDefault();
 
 		setEnteredNameTouched(true)
 
-		if (enteredName.trim() === "") {
-			setEnteredNameIsValid(false);
-			return;
-		}
+	
 
 		console.log(enteredName);
 
@@ -45,6 +54,7 @@ const SimpleInput = (props) => {
 					type="text"
 					id="name"
 					onChange={nameInputChangeHandler}
+					onBlur={nameInputBlurHandler}
 					value={enteredName}
 				/>
 				{nameInputIsInvalid && (

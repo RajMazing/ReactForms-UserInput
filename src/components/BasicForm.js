ce5951928e536ca9247590b4c108ce90d22b4eeb
RajMazing <caseyraj92@gmail.com>
2022-07-18 22:10:50 -0500

    BasicForm component now handles state when user inputs an empty string. howeever there is a bug after submitting a form. the error diplays even when passing validation checks

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,69 +1,118 @@
-import { useRef, useState } from 'react'
-
-
+import { useRef, useState } from "react";
 
 const BasicForm = (props) => {
-  const inputRefName = useRef();
-  const inputRefLastName = useRef()
-  const inputRefEmail = useRef()
+	const inputRefName = useRef();
+	const inputRefLastName = useRef();
+	const inputRefEmail = useRef();
 
-  const [ nameEntered, setNameEntered ] = useState();
-  const [ lastNameEntered, setLastNameEntered ] = useState()
-  const [ emailEntered, setEmailEntered ] = useState()
+	const [nameEntered, setNameEntered] = useState("");
+	const [lastNameEntered, setLastNameEntered] = useState("");
+	const [emailEntered, setEmailEntered] = useState("");
+	const [validName, setValidName] = useState(false);
+	const [validLastName, setValidLastName] = useState(false);
+	const [validEmail, setValidEmail] = useState(false);
 
+	const [nameTouched, setNameTouched] = useState(false);
+	const [lastNameTouched, setLastNameTouched] = useState(false);
+	const [emailTouched, setEmailTouched] = useState(false);
 
-  const inputNameChangeHandler = event => {
-    setNameEntered(event.target.value)
+	const inputNameChangeHandler = (event) => {
+		setNameEntered(event.target.value);
+	};
+
+	const inputLastNameChangeHandler = (event) => {
+		setLastNameEntered(event.target.value);
+	};
+
+	const inputEmailChangeHandler = (event) => {
+		setEmailEntered(event.target.value);
+	};
+
+	const formSubmission = (event) => {
+		event.preventDefault();
+
+		setNameTouched(true)
+    setLastNameTouched(true)
+    setEmailTouched(true)
     
-  }
+		if (nameEntered.trim() === "") {
+			setValidName(false);
+			return;
+		} else if (lastNameEntered.trim === "") {
+			setValidLastName(false);
+			return;
+		} else if (emailEntered.trim === "") {
+			setValidEmail(false);
+			return;
+		}
 
-  const inputLastNameChangeHandler = event => {
-    
-    setLastNameEntered(event.target.value)
-    
-  }
+		// const newNameValue = inputRefName.current.value;
+		// const newLastNameValue = inputRefLastName.current.value;
+		// const newEmailValue = inputRefEmail.current.value;
 
-  const inputEmailChangeHandler = event => {
-   
-    setEmailEntered(event.target.value)
-  }
+		// console.log(newNameValue, newLastNameValue, newEmailValue)
 
-  const formSubmission = event => {
-    event.preventDefault();
+		setNameEntered("");
+		setLastNameEntered("");
+		setEmailEntered("");
+	};
 
-    console.log(nameEntered, lastNameEntered, emailEntered)
+	const nameInputIsInvalid = !validName && nameTouched;
+	const lastNameInputIsInvalid = !validLastName && lastNameTouched;
+	const emailInputIsInvalid = !validEmail && emailTouched;
 
-    const newNameValue = inputRefName.current.value;
-    const newLastNameValue = inputRefLastName.current.value;
-    const newEmailValue = inputRefEmail.current.value;
+	const enteredNameInput = validName ? "form-control" : "form-control invalid";
+	const enteredLastNameInput = validLastName
+		? "form-control"
+		: "form-control invalid";
+	const enteredEmailInput = validEmail
+		? "form-control"
+		: "form-control invalid";
 
-
-    console.log(newNameValue, newLastNameValue, newEmailValue)
-  }
-
-
-
-  return (
-    <form onSubmit={formSubmission}>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input ref={inputRefName} type='text' id='name' onChange={inputNameChangeHandler} />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input ref={inputRefLastName} type='text' id='lastname' onChange={inputLastNameChangeHandler} />
-        </div>
-      </div>
-      <div className='form-control'>
-        <label htmlFor='email'>E-Mail Address</label>
-        <input ref={inputRefEmail} type='email' id='email' onChange={inputEmailChangeHandler} />
-      </div>
-      <div className='form-actions'>
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={formSubmission}>
+			<div className="control-group">
+				<div className={enteredNameInput}>
+					<label htmlFor="name">First Name</label>
+					<input
+						ref={inputRefName}
+						type="text"
+						id="name"
+						onChange={inputNameChangeHandler}
+						value={nameEntered}
+					/>
+					{nameInputIsInvalid && <p className="error-text">Name cannot be empty!</p>}
+				</div>
+				<div className={enteredLastNameInput}>
+					<label htmlFor="name">Last Name</label>
+					<input
+						ref={inputRefLastName}
+						type="text"
+						id="lastname"
+						onChange={inputLastNameChangeHandler}
+						value={lastNameEntered}
+					/>
+					{lastNameInputIsInvalid && (
+						<p className="error-text">Last Name cannot be empty!</p>
+					)}
+				</div>
+			</div>
+			<div className={enteredEmailInput}>
+				<label htmlFor="email">E-Mail Address</label>
+				<input
+					ref={inputRefEmail}
+					type="email"
+					id="email"
+					onChange={inputEmailChangeHandler}
+					value={emailEntered}
+				/>
+				{emailInputIsInvalid && <p className="error-text">Email cannot be empty!</p>}
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default BasicForm;

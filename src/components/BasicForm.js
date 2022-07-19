7c4acf376a371945dc52db46649b23a4b085d119
RajMazing <caseyraj92@gmail.com>
2022-07-18 21:23:05 -0500

    using React hooks useRef and useState to input information to the console. next step is to create a if condition to validate input for the name, last name, and email

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,19 +1,63 @@
+import { useRef, useState } from 'react'
+
+
+
 const BasicForm = (props) => {
+  const inputRefName = useRef();
+  const inputRefLastName = useRef()
+  const inputRefEmail = useRef()
+
+  const [ nameEntered, setNameEntered ] = useState();
+  const [ lastNameEntered, setLastNameEntered ] = useState()
+  const [ emailEntered, setEmailEntered ] = useState()
+
+
+  const inputNameChangeHandler = event => {
+    setNameEntered(event.target.value)
+    
+  }
+
+  const inputLastNameChangeHandler = event => {
+    
+    setLastNameEntered(event.target.value)
+    
+  }
+
+  const inputEmailChangeHandler = event => {
+   
+    setEmailEntered(event.target.value)
+  }
+
+  const formSubmission = event => {
+    event.preventDefault();
+
+    console.log(nameEntered, lastNameEntered, emailEntered)
+
+    const newNameValue = inputRefName.current.value;
+    const newLastNameValue = inputRefLastName.current.value;
+    const newEmailValue = inputRefEmail.current.value;
+
+
+    console.log(newNameValue, newLastNameValue, newEmailValue)
+  }
+
+
+
   return (
-    <form>
+    <form onSubmit={formSubmission}>
       <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input ref={inputRefName} type='text' id='name' onChange={inputNameChangeHandler} />
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input ref={inputRefLastName} type='text' id='lastname' onChange={inputLastNameChangeHandler} />
         </div>
       </div>
       <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <label htmlFor='email'>E-Mail Address</label>
+        <input ref={inputRefEmail} type='email' id='email' onChange={inputEmailChangeHandler} />
       </div>
       <div className='form-actions'>
         <button>Submit</button>
